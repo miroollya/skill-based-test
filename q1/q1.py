76cec730204ddc4c37c55e0a08f5b0f69e3be5d8
llya <2021454448@student.uitm.edu.my>
2023-07-20 03:23:52 +0000

    latest

--- q1/q1.py
+++ q1/q1.py
@@ -5,25 +5,25 @@ def main():
     server_port = 8443
 
     try:
-        # Create a TCP/IP socket
+        
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Connect to the server
+       
         client_socket.connect((server_address, server_port))
 
         print("Connected to the server!")
 
-        # Replace 'YOUR_UITM_STUDENT_ID' with your actual UiTM Student ID
+        
         student_id = "2021454448"
 
-        # Send the Student ID to the server
+       
         client_socket.sendall(student_id.encode())
 
-        # Receive the server's response
+       
         response = client_socket.recv(1024).decode()
         print("Server response:", response)
 
-        # Close the socket connection
+       
         client_socket.close()
 
     except Exception as e:
